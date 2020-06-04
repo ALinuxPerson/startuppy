46c0ccb6cea7cdf4f166c434fb40260e3780f086
Michael Baterna <micheal02052007@gmail.com>
2020-06-04 20:31:41 +0800

    Add init systems to StartupRemove, edit StartupRemove function signature

--- startuppy/remove.py
+++ startuppy/remove.py
@@ -1,15 +1,27 @@
 class StartupRemove:
-    def remove(self):
+    def remove(self, command: str):
         raise NotImplementedError
 
+class SystemDLinuxAdd(StartupRemove):
+    def remove(self, command: str):
+        pass
+
+class UpstartLinuxAdd(StartupRemove):
+    def remove(self, command: str):
+        pass
+
+class SysVInitLinuxAdd(StartupRemove):
+    def remove(self, command: str):
+        pass
+
 class LinuxRemove(StartupRemove):
-    def remove(self):
+    def remove(self, command: str):
         pass
 
 class MacRemove(StartupRemove):
-    def remove(self):
+    def remove(self, command: str):
         pass
 
 class WindowsRemove(StartupRemove):
-    def remove(self):
+    def remove(self, command: str):
         pass
