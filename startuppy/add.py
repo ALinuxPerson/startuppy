c4fc4a0f62e472ec74876f26ea6bce782ef912a9
Michael Baterna <micheal02052007@gmail.com>
2020-06-04 20:29:10 +0800

    Add different linux init systems
    1. Add different linux init systems
    2. Remove python-crontab from requirements.txt

--- startuppy/add.py
+++ startuppy/add.py
@@ -1,15 +1,23 @@
 class StartupAdd:
-    def add(self):
+    def add(self, command: str):
         raise NotImplementedError
 
-class LinuxAdd(StartupAdd):
-    def add(self):
+class SystemDLinuxAdd:
+    def add(self, command: str):
+        pass
+
+class UpstartLinuxAdd:
+    def add(self, command: str):
+        pass
+
+class SysVInitLinuxAdd:
+    def add(self, command: str):
         pass
 
 class MacAdd(StartupAdd):
-    def add(self):
+    def add(self, command: str):
         pass
 
 class WindowsAdd(StartupAdd):
-    def add(self):
+    def add(self, command: str):
         pass
