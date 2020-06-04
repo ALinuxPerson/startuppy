5573297117bee2652cc5c988a74f8caab5f30932
Michael Baterna <micheal02052007@gmail.com>
2020-06-04 20:48:45 +0800

    Reimplement Linux as a redirect for init systems

--- startuppy/add.py
+++ startuppy/add.py
@@ -1,3 +1,6 @@
+from typing import *
+import os
+
 class StartupAdd:
     def add(self, command: str):
         raise NotImplementedError
@@ -13,6 +16,38 @@ class UpstartLinuxAdd(StartupAdd):
 class SysVInitLinuxAdd(StartupAdd):
     def add(self, command: str):
         pass
+
+class LinuxAdd(StartupAdd):
+    @property
+    def _init_system(self):
+        switch_case: Dict[str, bool] = {
+            "systemd": os.path.exists("/usr/lib/systemd"),
+            "upstart": os.path.exists("/usr/share/upstart"),
+            "sysvinit": os.path.exists("/etc/init.d")
+        }
+
+        for init_system, check in switch_case.items():
+            if check:
+                return init_system
+        else:
+            raise SystemError("init is unknown")
+
+    def add(self, command: str):
+        init_system: str = self._init_system
+
+        systemd: SystemDLinuxAdd = SystemDLinuxAdd()
+        upstart: UpstartLinuxAdd = UpstartLinuxAdd()
+        sysvinit: SysVInitLinuxAdd = SysVInitLinuxAdd()
+
+        switch_case: Dict[str, Callable] = {
+            "systemd": systemd.add,
+            "upstart": upstart.add,
+            "sysvinit": sysvinit.add
+        }
+
+        for init_system_name, func in switch_case.items():
+            if init_system_name == init_system:
+                func(command)
 
 class MacAdd(StartupAdd):
     def add(self, command: str):
--- startuppy/remove.py
+++ startuppy/remove.py
@@ -1,18 +1,49 @@
+from typing import *
+import os
+
 class StartupRemove:
     def remove(self, command: str):
         raise NotImplementedError
 
-class SystemDLinuxAdd(StartupRemove):
+class SystemDLinuxRemove(StartupRemove):
     def remove(self, command: str):
         pass
 
-class UpstartLinuxAdd(StartupRemove):
+class UpstartLinuxRemove(StartupRemove):
     def remove(self, command: str):
         pass
 
-class SysVInitLinuxAdd(StartupRemove):
+class SysVInitLinuxRemove(StartupRemove):
+    @property
+    def _init_system(self):
+        switch_case: Dict[str, bool] = {
+            "systemd": os.path.exists("/usr/lib/systemd"),
+            "upstart": os.path.exists("/usr/share/upstart"),
+            "sysvinit": os.path.exists("/etc/init.d")
+        }
+
+        for init_system, check in switch_case.items():
+            if check:
+                return init_system
+        else:
+            raise SystemError("init is unknown")
+
     def remove(self, command: str):
-        pass
+        init_system: str = self._init_system
+
+        systemd: SystemDLinuxRemove = SystemDLinuxRemove()
+        upstart: UpstartLinuxRemove = UpstartLinuxRemove()
+        sysvinit: SysVInitLinuxRemove = SysVInitLinuxRemove()
+
+        switch_case: Dict[str, Callable] = {
+            "systemd": systemd.remove,
+            "upstart": upstart.remove,
+            "sysvinit": sysvinit.remove
+        }
+
+        for init_system_name, func in switch_case.items():
+            if init_system_name == init_system:
+                func(command)
 
 class MacRemove(StartupRemove):
     def remove(self, command: str):
