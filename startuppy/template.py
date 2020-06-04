7639096b109519b66f8d376026b775313f9b0ac3
Michael Baterna <micheal02052007@gmail.com>
2020-06-04 18:23:20 +0800

    Add methods to template.py
    1. Rearrange imports in template.py
    2. Add methods to template.py

--- startuppy/template.py
+++ startuppy/template.py
@@ -1,7 +1,13 @@
-from startuppy.add import StartupAdd
 from startuppy.remove import StartupRemove
+from startuppy.add import StartupAdd
 
 class Startup:
     def __init__(self, add: StartupAdd, remove: StartupRemove):
         self._add: StartupAdd = add
         self._remove: StartupRemove = remove
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
