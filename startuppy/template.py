85efc04f246642b40c09dbb99008fc3969ef3b7a
Michael Baterna <micheal02052007@gmail.com>
2020-06-04 21:06:55 +0800

    Add command argument to template.py:Startup

--- startuppy/template.py
+++ startuppy/template.py
@@ -2,12 +2,13 @@ from startuppy.remove import StartupRemove
 from startuppy.add import StartupAdd
 
 class Startup:
-    def __init__(self, add: StartupAdd, remove: StartupRemove):
+    def __init__(self, add: StartupAdd, remove: StartupRemove, command: str):
         self._add: StartupAdd = add
         self._remove: StartupRemove = remove
+        self.command: str = command
 
-    def add(self):
+    def add(self, command: str):
         pass
 
-    def remove(self):
+    def remove(self, command: str):
         pass
