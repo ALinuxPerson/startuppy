02b406813434a9f1834fa337dc4081eba5de6d8c
Michael Baterna <micheal02052007@gmail.com>
2020-06-04 21:11:41 +0800

    Add absolute path to template.py:Startup:command

--- startuppy/template.py
+++ startuppy/template.py
@@ -1,11 +1,12 @@
 from startuppy.remove import StartupRemove
 from startuppy.add import StartupAdd
+import os
 
 class Startup:
     def __init__(self, add: StartupAdd, remove: StartupRemove, command: str):
         self._add: StartupAdd = add
         self._remove: StartupRemove = remove
-        self.command: str = command
+        self.command: str = os.path.abspath(command)
 
     def add(self, command: str):
         pass
