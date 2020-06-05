62f27b9b04cd20ba66f4e9e9c03e56ada09c3d6c
Michael Baterna <micheal02052007@gmail.com>
2020-06-05 16:25:24 +0800

    Argument changes for cli.py
    1. Encapsulate arguments to a function
    2. Separate parser and args from each other

--- startuppy/cli.py
+++ startuppy/cli.py
@@ -1,15 +1,21 @@
+from typing import *
 import argparse
+import sys
 
 parser: argparse.ArgumentParser = argparse.ArgumentParser(description="StartupPy CLI")
-group = parser.add_mutually_exclusive_group()
-group.add_argument(
-    "-a",
-    "--add",
-    help="adds a command to startup."
-)
-group.add_argument(
-    "-r",
-    "--remove",
-    help="removes a command from startup."
-)
-arguments = parser.parse_args()
+
+def args(arguments: List[str] = None) -> argparse.Namespace:
+    if arguments is None:
+        arguments: List[str] = sys.argv[1:]
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-a",
+        "--add",
+        help="adds a command to startup."
+    )
+    group.add_argument(
+        "-r",
+        "--remove",
+        help="removes a command from startup."
+    )
+    return parser.parse_args(arguments)
