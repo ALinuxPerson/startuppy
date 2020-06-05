7f580ad9c6d3eda80dd991c80c7662864b94e520
Michael Baterna <micheal02052007@gmail.com>
2020-06-05 16:53:17 +0800

    Add 'required=True' argument to cli.py:args:group to prevent TypeError

--- startuppy/cli.py
+++ startuppy/cli.py
@@ -9,7 +9,7 @@ parser: argparse.ArgumentParser = argparse.ArgumentParser(description="StartupPy
 def args(arguments: List[str] = None) -> argparse.Namespace:
     if arguments is None:
         arguments: List[str] = sys.argv[1:]
-    group = parser.add_mutually_exclusive_group()
+    group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument(
         "-a",
         "--add",
